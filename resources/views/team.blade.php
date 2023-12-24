2e7d0f6f37ca59c45441d51513b52c92818ee665
Sara Mohamed <skyangelsara@gmail.com>
2023-12-25 00:19:26 +0200

    laravel-R4

---
+++ resources/views/team.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        @include('includes.header')
+
+
+        <!-- Page Header End -->
+        <div class="container-xxl py-5 page-header position-relative mb-5">
+            <div class="container py-5">
+                <h1 class="display-2 text-white animated slideInDown mb-4">Teachers</h1>
+                <nav aria-label="breadcrumb animated slideInDown">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="breadcrumb-item text-white active" aria-current="page">Teachers</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        @include('includes.team')
+
+
+        @include('includes.footer')
+</body>
+
+</html>
---
+++ resources/views/testomonial.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        @include('includes.header')
+
+
+        <!-- Page Header End -->
+        <div class="container-xxl py-5 page-header position-relative mb-5">
+            <div class="container py-5">
+                <h1 class="display-2 text-white animated slideInDown mb-4">Testimonial</h1>
+                <nav aria-label="breadcrumb animated slideInDown">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="breadcrumb-item text-white active" aria-current="page">Testimonial</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+
+        @include('includes.testimonial')
+
+        @include('includes.footer')
+</body>
+
+</html>
